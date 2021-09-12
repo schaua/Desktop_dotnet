9db63393ec20afc15d48c5b3651cf6bf56730fff
Augie Schau <schaua@augustschau.com>
2021-09-12 13:34:46 -0400

    Add No XAML Demo

---
+++ Day1/HelloWPF/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HelloWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            this.Startup += MyStartUp;
+        }
+
+        private static void MyStartUp(object sender, StartupEventArgs e)
+        {
+            Window window = new Window();
+            window.Height = 300;
+            window.Width = 300;
+            window.Title = "Hello WPF";
+
+            window.Show();
+        }
+    }
+}
